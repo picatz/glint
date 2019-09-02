8020a528d8f09d433c24322464b5d616eeaddf1d
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-09-02 16:37:21 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
 	"os"
+
+	"golang.org/x/tools/go/loader"
 )
 
+var (
+	rulesFile  string
+	targetCode []string
+)
+
+func init() {
+	flag.StringVar(&rulesFile, "rules", "Glintfile", "the JSON configuation files for glint")
+	flag.Parse()
+	targetCode = flag.Args()
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
 func main() {
-	rulesIndex, err := NewRulesIndex("rules.json")
+	rulesIndex, err := NewRulesIndex(rulesFile)
 
 	if err != nil {
 		panic(err)
@@ -23,18 +45,42 @@ func main() {
 
 	fs := token.NewFileSet()
 
-	for _, arg := range os.Args[1:] {
-		f, err := parser.ParseFile(fs, arg, nil, parser.AllErrors)
-		if err != nil {
-			log.Printf("could not parse %s: %v", arg, err)
-			continue
-		}
+	conf := loader.Config{
+		Fset: fs,
+	}
 
-		ast.Inspect(f, func(n ast.Node) bool {
-			for _, rule := range rulesIndex.Rules {
-				rule.Action(fs, n)
+	for _, arg := range targetCode {
+		if fileExists(arg) {
+			f, err := parser.ParseFile(fs, arg, nil, parser.AllErrors)
+			if err != nil {
+				log.Printf("could not parse %s: %v", arg, err)
+				continue
 			}
-			return true
-		})
+			ast.Inspect(f, func(n ast.Node) bool {
+				for _, rule := range rulesIndex.Rules {
+					rule.Action(fs, n)
+				}
+				return true
+			})
+		} else {
+			conf.Import(arg)
+		}
+	}
+
+	lprog, err := conf.Load()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	for _, programInfo := range lprog.InitialPackages() {
+		for _, file := range programInfo.Files {
+			ast.Inspect(file, func(n ast.Node) bool {
+				for _, rule := range rulesIndex.Rules {
+					rule.Action(fs, n)
+				}
+				return true
+			})
+		}
 	}
 }
